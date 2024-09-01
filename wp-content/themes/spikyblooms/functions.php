a947243e3897089066fbcd5ce4795327f857f097
keatonmurray <murraykeaton422@gmail.com>
2024-09-01 11:34:00 -0400

    improvised template layout

--- wp-content/themes/spikyblooms/functions.php
+++ wp-content/themes/spikyblooms/functions.php
@@ -1,21 +1,11 @@
 <?php
 
     function spiky_blooms_enqueue_scripts() {
-        // Enqueue Bootstrap CSS
         wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css', array(), '5.3.3');
-        
-        // Enqueue FontAwesome
         wp_enqueue_script('font-awesome', 'https://kit.fontawesome.com/5dfbc71075.js', array(), null, true);
-        
-        // Enqueue Google Fonts
         wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap', array(), null);
-        
-        // Enqueue theme stylesheet
         wp_enqueue_style('theme-styles', get_stylesheet_uri());
-
         wp_enqueue_style('custom-styles', get_template_directory_uri() . '/assets/css/custom.css', array('theme-styles'), '1.0');
-        
-        // Enqueue Bootstrap JS
         wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js', array(), '5.3.3', true);
     }
 
@@ -30,6 +20,6 @@
     }
 
     add_action('after_setup_theme', 'spiky_blooms_setup');
-
+    
     add_filter('show_admin_bar', '__return_false');
 
--- wp-content/themes/spikyblooms/index.php
+++ wp-content/themes/spikyblooms/index.php
@@ -16,45 +16,5 @@
                 </div>
             </div>
             <br><br>
-            <div id="products" class="mt-4">
-                    <div class="row">
-                        <div class="products col-12 col-md-3">
-                           <div class="product-img-container text-center d-flex flex-column align-items-center"> 
-                                <figure>
-                                    <img src="<?php echo get_template_directory_uri();?>/assets/images/cactus1.jpg" alt="Product Image" class="img-fluid">
-                                </figure>
-                                <p class="product-title">Corsage Cactus</p>
-                                <h3 class="product-price">$12.50</h3>
-                            </div>
-                        </div>
-                        <div class="products col-12 col-md-3">
-                            <div class="product-img-container text-center d-flex flex-column align-items-center"> 
-                                <figure>
-                                    <img src="<?php echo get_template_directory_uri();?>/assets/images/cactus1.jpg" alt="Product Image">
-                                </figure>
-                                <p class="product-title">Corsage Cactus</p>
-                                <h3 class="product-price">$12.50</h3>
-                             </div>
-                         </div>
-                         <div class="products col-12 col-md-3">
-                            <div class="product-img-container text-center d-flex flex-column align-items-center"> 
-                                <figure>
-                                    <img src="<?php echo get_template_directory_uri();?>/assets/images/cactus1.jpg" alt="Product Image">
-                                </figure>
-                                <p class="product-title">Corsage Cactus</p>
-                                <h3 class="product-price">$12.50</h3>
-                             </div>
-                         </div>
-                         <div class="products col-12 col-md-3">
-                            <div class="product-img-container text-center d-flex flex-column align-items-center"> 
-                                <figure>
-                                    <img src="<?php echo get_template_directory_uri();?>/assets/images/cactus1.jpg" alt="Product Image">
-                                </figure>
-                                <p class="product-title">Corsage Cactus</p>
-                                <h3 class="product-price">$12.50</h3>
-                             </div>
-                         </div>
-                    </div>
-                </div>
-            
+            <?php echo get_template_part('templates/products'); ?>
 <?php get_footer(); ?>
